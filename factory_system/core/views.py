76b4b373b574fffbd7c0201b2d8d0d2d76b9d55e
jordanjw404 <jwilliamson1994@outlook.com>
2025-04-22 11:40:31 +0100

    Added templates to core

--- factory_system/core/views.py
+++ factory_system/core/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+
+# Create User views here.
+def UserView(request):
+    users = User.objects.all()
+    password = 'password'
+    hashed_password = make_password(password)
+    return render(request, 'create_user.html', 
+                {'users': users, 'hashed_password': hashed_password})
--- factory_system/factory_system/settings.py
+++ factory_system/factory_system/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "django_bootstrap5",
+    "core",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+SESSION_COOKIE_SECURE = True 
+SESSION_COOKIE_HTTPONLY = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
