302d9c75a48b2638573bae47c68a50f58ef2dd6e
jordanjw404 <jordannjww@gmail.com>
2025-06-18 15:33:46 +0100

    Add employees and factory_kpis apps; create initial files and register in settings

--- factory_system/factory_kpis/models.py
+++ factory_system/factory_kpis/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+<<<<<<< HEAD
 class build_kpi(models.Model):
     build_id = models.CharField(max_length=100, unique=True)
     build_time = models.FloatField()
@@ -11,3 +12,6 @@ class build_kpi(models.Model):
         return f"Build KPI {self.build_id}"
     
     
+=======
+class Build
+>>>>>>> 43fa2f6 (Add employees and factory_kpis apps; create initial files and register in settings)
