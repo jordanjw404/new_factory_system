8fc4a147330d32acc25c609dfd896bfbe6412bba
jordanjw404 <jordannjww@gmail.com>
2025-07-01 12:54:23 +0100

    Add production automation, target/completion dates, and UI updates

--- factory_system/factory_kpis/models.py
+++ factory_system/factory_kpis/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 1a65426 (Add production automation, target/completion dates, and UI updates)
 class build_kpi(models.Model):
     build_id = models.CharField(max_length=100, unique=True)
     build_time = models.FloatField()
@@ -11,7 +14,11 @@ class build_kpi(models.Model):
     def __str__(self):
         return f"Build KPI {self.build_id}"
     
+<<<<<<< HEAD
     
 =======
 class Build
 >>>>>>> 43fa2f6 (Add employees and factory_kpis apps; create initial files and register in settings)
+=======
+    
+>>>>>>> 1a65426 (Add production automation, target/completion dates, and UI updates)
