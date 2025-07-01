c53d700f4a76e13e95272fc7e2f3022797dc9670
jordanjw404 <81982783+jordanjw404@users.noreply.github.com>
2025-07-01 13:38:46 +0100

    Add HttpResponse import

--- factory_system/inventory/views.py
+++ factory_system/inventory/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
+from django.http import HttpResponse
 from .models import Inventory
 
 def inventory_list_view(request):
